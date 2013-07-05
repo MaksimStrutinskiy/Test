abf4c146f817f68813531f5ba1735611aa72ed1a
Maksim Strytinskiy <Ator@anton-bludov.teamdev.local>
2013-07-05 13:18:56 +0300

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Maksim Strytinskiy on 7/5/13.
+//  Copyright (c) 2013 Maksim Strytinskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Maksim Strytinskiy on 7/5/13.
+//  Copyright (c) 2013 Maksim Strytinskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
