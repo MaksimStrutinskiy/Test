04e0102d1d78ec230dac88bfe4c37a6c4740708b
Maksim Strytinskiy <Ator@anton-bludov.teamdev.local>
2013-07-05 13:31:41 +0300

    Added SomeClass to project.

---
+++ GitUseExample/SomeClass.h
@@ -0,0 +1,13 @@
+//
+//  SomeClass.h
+//  GitUseExample
+//
+//  Created by Maksim Strytinskiy on 7/5/13.
+//  Copyright (c) 2013 Maksim Strytinskiy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SomeClass : NSObject
+
+@end
---
+++ GitUseExample/SomeClass.m
@@ -0,0 +1,13 @@
+//
+//  SomeClass.m
+//  GitUseExample
+//
+//  Created by Maksim Strytinskiy on 7/5/13.
+//  Copyright (c) 2013 Maksim Strytinskiy. All rights reserved.
+//
+
+#import "SomeClass.h"
+
+@implementation SomeClass
+
+@end
